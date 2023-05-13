2348d162dd694ffdac5e919c50bf32ecd90877d8
RobinCoder010 <132834321+RobinCoder010@users.noreply.github.com>
2023-05-13 09:23:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://DJmaster:djmaster@cluster0.2an0b.mongodb.net/?retryWrites=true&w=majority',//database link
+    
+    bot_token:'6116632629:AAEq8_zlReo_zoi11JN5MM7NNVxIgcXe25M',//bot token
+    
+    bot_name:'PolydogeAirdropRound1_Bot',//bot username without @
+    
+    bot_admin:5602830736,// admin telegram id
+    
+    channelsList : ['@LegitAirdrops02'],// channels list
+    
+    reffer_bonus:100000,// refferal bonus amount
+    
+    min_wd:500000,//min withdrawal amount
+    
+    daily_bonus:0,// daily bonus amount
+    
+    currency:'PolyDoge', //bot currency
+    
+    contractaddress : '0x8A953CfE442c5E8855cc6c61b1293FA648BAE472', //contract address of coin
+    
+    privateKey : '3fc3a7329c9ae5e530ee22b47a4b311de05986c127ab44b0d6be436b051287ec', //private key of wallet
+    
+    address : '0x8423B3CAE65C727797F6951A55106fC082514050',//polygon address of wallet
+    
+    payment_channel:'@BotsPayments_in'// payment channel username
+    
+    }
